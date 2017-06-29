7ce86ca6904b68c2ab33bab03a567b950a09285d
Anubis523 <anubis523@gmail.com>
2017-06-28 20:37:55 -0400

    Camera movement and some logic fixes
    made camera that follows, tweaked flip and other movement stuff as well

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+  
+	//this camera
+	private Camera camera;
+	//player as target
+	public Transform target;
+	//distance b/n camera and player
+	public Vector3 offset ;
+
+	public float smoothSpeed = 0.125f;
+
+	void Awake (){
+		camera = GetComponent<Camera> ();
+		offset = Vector3.forward * -7.5f;
+
+	}
+	// Update is called once per frame after Update
+	void FixedUpdate () {
+		Vector3 desiredPosition = target.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp (transform.position,desiredPosition,smoothSpeed);
+		transform.position = smoothedPosition;
+		}
+
+}
