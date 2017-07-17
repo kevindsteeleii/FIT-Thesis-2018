c7cc44bc34132b2230880e8f683ef34682175c9a
Anubis523 <anubis523@gmail.com>
2017-07-16 23:48:09 -0400

    slight tweaks and input manager

--- Assets/Scripts/Components/KeyboardInputComponent.cs
+++ Assets/Scripts/Components/KeyboardInputComponent.cs
@@ -3,7 +3,7 @@
 public class KeyboardInputComponent : MonoBehaviour {
     public KeyCode keyCode = KeyCode.A;
 
-    private void Awake () {
+    private void Start () {
         KeyboardInputManager.instance.Subscribe(this.keyCode);
     }
 }
