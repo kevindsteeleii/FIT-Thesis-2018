7b18925150f7aaaea4e3515820290d73b083041e
Anubis523 <anubis523@gmail.com>
2017-07-17 14:38:42 -0400

    FakeDeath Orientation fix
    fixes respawn while facing opposite of initial spawn of Player Char

--- Assets/Scripts/death.cs
+++ Assets/Scripts/death.cs
@@ -6,26 +6,15 @@ public class death : MonoBehaviour {
 
 	Animator myAnim;
 	Rigidbody myRB;
-	Vector3 respawnPos;
-	Quaternion rot;
 
 	// Use this for initialization
 	void Awake () {
+
 		myRB = GetComponent <Rigidbody> ();
 		myAnim = GetComponent <Animator> ();
-		respawnPos = myRB.transform.position;
-		rot = myRB.transform.rotation;
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		//some code that just creates a back to life to undo the false death state
-		if (Input.GetKey (KeyCode.Space)&&myAnim.GetBool("dead")==true) {
-			transform.SetPositionAndRotation (respawnPos, rot);
-			myAnim.SetBool ("dead", false);
-			myAnim.SetBool ("grounded", true);
-		}
-	}
+
 	//false death-state upon contact with insta-Kill death object, just triggers death animation and ceases other ones
 	void OnCollisionEnter (Collision col){
 		if (col.gameObject.tag == "Death Object") {
