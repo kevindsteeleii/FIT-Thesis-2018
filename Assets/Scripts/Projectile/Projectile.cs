029bec5e13569adfef20bcfad5c0b385b2437696
Anubis523 <anubis523@gmail.com>
2017-07-24 21:01:16 -0400

    ProjetilesAdded
    Beginning to write the projectile modules

---
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : Model {
+	//Class used to create projectile with its own velocity
+
+	//damage the projectile causes
+	[SerializeField]
+	protected float damage = 5f;
+	// direction of the projectile
+	[SerializeField]
+	protected Vector3 direction;
+
+	[SerializeField]
+	protected AimProxyModel reticle;
+
+	void Awake () {
+//		r
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	//My game is about robots though too
+}
---
+++ Assets/Scripts/Projectile/ProjectileController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : Controller {
+
+	[SerializeField]
+	private Projectile projectile;
+
+	[SerializeField]
+	private List <Projectile> ammo;
+	protected virtual void Awake () {
+		//check to see if projectile is there
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
