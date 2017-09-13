b24065d03fc51e3b390494edc088c7662659a251
Anubis523 <anubis523@gmail.com>
2017-09-12 20:42:53 -0400

    tweaks more

---
+++ Assets/Scripts/AimProxy/AimProxyPlus.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class AimProxyPlus : MonoBehaviour {
+
+    [SerializeField]
+    protected AimProxyModel model;
+
+    protected virtual void Awake()
+    {
+        // check to see if the model variable is empty
+        if (!model)
+        {
+            // if it is then get the model attached to the current GameObject
+            model = this.gameObject.GetComponent<AimProxyModel>();
+        }
+    }
+    /*adds additional functionality outside of the keyboardinputparameters
+     it is triggered by the push and release of the input button labeled
+     as "Aim"*/
+       protected virtual void Update() { 
+        if(Input.GetButtonDown("Aim"))
+            {
+                model.StartProgressUpdateLoop();
+            }
+        
+       else if(Input.GetButtonUp("Aim"))
+            {
+                model.EndProgressUpdateLoop();
+            }
+        }
+    }
+
