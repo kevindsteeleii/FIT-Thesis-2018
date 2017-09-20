400f90a24d7dc3001656cb518b5fefa9f22c4dec
coopem2 <coopem2@gmail.com>
2017-09-20 17:54:09 -0400

    you saw this shit

---
+++ Assets/Scripts/Camera and UI/BulletControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletControl : MonoBehaviour {
+	public Sprite[] Shots;
+    public Image BulletImage;
+	float test;
+	
+	void Update(){
+		BulletImage.sprite = Shots[Ammo.instance.bullets];
+	}
+}
