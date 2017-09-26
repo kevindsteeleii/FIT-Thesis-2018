1b759fbf055ea0fe3acbd42964601b168b045a4b
coopem2 <coopem2@gmail.com>
2017-09-26 17:23:54 -0400

    made this branch
    testing the implimentation of the pause menu

---
+++ Assets/Scripts/Camera and UI/PauseGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems; 
+
+public class PauseGame : MonoBehaviour {
+	public GameObject menu;
+	public bool visable;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	public void Onclick(){
+		Debug.Log("Clicked");
+		visable=false;
+		menu.SetActive(false);
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)||Input.GetKeyDown(KeyCode.Space)){
+			if (visable== true){
+				visable = false;
+			}
+			else if (visable == false){
+				visable = true;
+			}
+            menu.SetActive(visable);
+		}			
+	}
+}
