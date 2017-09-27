5660fd017edbf7ae3f88f4dec3264a103ae861de
coopem2 <coopem2@gmail.com>
2017-09-26 20:56:12 -0400

    Made a pause menu
    added a game object and immage, immage contains a vertical layout and 3 buttons, unpause, restart, quit.
    
    created C# code 'pause menue' which creates keyboard inputs for ESC and space keys to change state of boolean 'paused' and sets the gamestate/visibility of the menu game object accordingly.
    created two additional scripts named unpause() and restart().
    Unpause sets paused = fasle and sets game speed to 1. Restart loads the current game state and sets gamespeed = 1
    
    following are notes about other things i had to do to make this shit work
    
    Enabled the EventSystem's Event Sysytem (script)
    add 'Submit' to the input settings
    attach pause game script to Game Manager, and match Menu to the Game object 'Menu' in UI
    In UI, under Unpause, add an On Click Event, drag game mannager into the empty feild. From the right drop down menu select PauseGame > Pausegame.unpause
    Repeat above for restart, selecting Restart

--- Assets/Scripts/Camera and UI/PauseGame.cs
+++ Assets/Scripts/Camera and UI/PauseGame.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems; 
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class PauseGame : MonoBehaviour {
 	public GameObject menu;
@@ -12,10 +13,15 @@ public class PauseGame : MonoBehaviour {
 	void Start () {
 
 	}
-	public void Onclick(){
+	public void unpause(){
 		Debug.Log("Clicked");
 		visable=false;
 		menu.SetActive(false);
+		Time.timeScale = 1;
+	}
+	public void restart(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		Time.timeScale = 1;
 	}
 		
 	// Update is called once per frame
@@ -23,9 +29,11 @@ public class PauseGame : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Escape)||Input.GetKeyDown(KeyCode.Space)){
 			if (visable== true){
 				visable = false;
+				Time.timeScale = 1;
 			}
 			else if (visable == false){
 				visable = true;
+				Time.timeScale = 0;
 			}
             menu.SetActive(visable);
 		}			
