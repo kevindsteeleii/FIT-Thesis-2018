b077df23de4cd602c2a6f5644aa1f4486e821f43
coopem2 <coopem2@gmail.com>
2018-04-25 15:37:21 -0400

    added a lava texture we discussed on discord a few weeks back

---
+++ Assets/platformStick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformStick : MonoBehaviour {
+	public GameObject platform;
+	public GameObject Player;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			Player.transform.parent = platform.transform;
+			Debug.Log ("plonk");
+		}
+	}
+}
