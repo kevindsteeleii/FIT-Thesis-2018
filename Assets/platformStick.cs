4160ddbee959969e76463fa174702ea2e68f9565
coopem2 <coopem2@gmail.com>
2018-04-25 19:02:03 -0400

    patroling platforms
    created patroling platforms that parent the player to them when they enter their collider, and unparent them when they exit.
    Began work on the factory level

--- Assets/platformStick.cs
+++ Assets/platformStick.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class platformStick : MonoBehaviour {
 	public GameObject platform;
-	public GameObject Player;
+
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player")
 		{
-			Player.transform.parent = platform.transform;
+			other.transform.parent =  platform.transform;
 			Debug.Log ("plonk");
 		}
+	}
+		
+	private void OnTriggerExit (Collider other){
+		other.transform.parent = null;
 	}
 }
