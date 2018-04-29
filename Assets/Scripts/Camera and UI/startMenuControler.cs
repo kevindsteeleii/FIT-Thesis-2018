a860695f254891f30c0f5e4e34f56e7829630936
coopem2 <coopem2@gmail.com>
2018-04-29 10:47:35 -0400

    made a script for the start menu
    doesnt have any real progamatic stuff, its just hooked up to the canvas in the main menu scene

---
+++ Assets/Scripts/Camera and UI/startMenuControler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startMenuControler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public virtual void gameStart(){
+		Debug.Log("Start");
+
+	}
+
+	public virtual void gameQuit(){
+		Debug.Log("Quit");
+	}
+
+}
