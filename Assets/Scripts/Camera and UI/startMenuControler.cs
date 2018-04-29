88d528d6b068fddcd4578cf71d5bbe257538797d
coopem2 <coopem2@gmail.com>
2018-04-29 13:21:18 -0400

    Start and Exits
    Made an 'Exit' asset and added it to the first and second level
    
    coded the main menu start button to launch the first level.

--- Assets/Scripts/Camera and UI/startMenuControler.cs
+++ Assets/Scripts/Camera and UI/startMenuControler.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class startMenuControler : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	public virtual void gameStart(){
 		Debug.Log("Start");
+		SceneManager.LoadScene ("20180416_level_001_MLC", LoadSceneMode.Single);
+		SceneManager.LoadScene ("20180418_level_001_Interactables_MLC", LoadSceneMode.Additive);
+		SceneManager.LoadScene ("20180420_PlayerSetUp_Anims_KDS", LoadSceneMode.Additive);
+		SceneManager.LoadScene ("20180426_level_001_Enemies", LoadSceneMode.Additive);
+		SceneManager.LoadScene ("UI_StandAlone_20180413_MLCmod", LoadSceneMode.Additive);
 
 	}
 
