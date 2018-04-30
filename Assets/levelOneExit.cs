ec3a80e7cc503a67fe17f1256c47fa1c0e72242d
coopem2 <coopem2@gmail.com>
2018-04-30 17:28:56 -0400

    bugfixes
    AAAAAAAAAAAAAAA

---
+++ Assets/levelOneExit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelOneExit : MonoBehaviour {
+
+	// Use this for initialization
+	private void OnTriggerEnter(Collider other){
+		if (other.tag == "Player"){
+			SceneManager.LoadScene ("20180425_level_002_MLC", LoadSceneMode.Single);
+			SceneManager.LoadScene ("20180427_level_002_Interactables_MLC", LoadSceneMode.Additive);
+			SceneManager.LoadScene ("20180420_PlayerSetUp_Anims_KDS", LoadSceneMode.Additive);
+			SceneManager.LoadScene ("20180427_level_002_Enemies", LoadSceneMode.Additive);
+			SceneManager.LoadScene ("UI_StandAlone_20180413_MLCmod", LoadSceneMode.Additive);
+		}
+	}
+}
