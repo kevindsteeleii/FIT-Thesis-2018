cfd1bcd592f0430c53d8f1263acb76a7f82d1ea8
coopem2 <coopem2@gmail.com>
2018-05-02 09:20:05 -0400

    the second level exit object

---
+++ Assets/levelTwoExit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelTwoExit : MonoBehaviour {
+
+	private void OnTriggerEnter(){
+		SceneManager.LoadScene ("20180425_Start Screen", LoadSceneMode.Single);
+		Debug.Log("zip");
+
+	}
+}
